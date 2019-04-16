fed0987b7f4972da629dbe69a9fead19eb30fc2b
iiSkyii <iiskyii@github.com>
2019-04-16 14:41:23 +0100

    Signed-off-by: iiSkyii <iiskyii@github.com>

--- ana.py
+++ ana.py
@@ -96,7 +96,7 @@ class bote(discord.Client):
                 return
             else:
                 kitchen = client.get_channel(567702425717178391)
-                await kitchen.send(message.author + ' has ordered '+args+' in <#'+channel.id+'>.')
+                await kitchen.send(message.author.name + ' has ordered '+args+' in <#'+channel.id+'>.')
                 await channel.send("? We'll get that to you ASAP!")				
             
 client = bote()
