0165527bfe5ee2485aaabecf5f63647714585269
iiSkyii <iiskyii@github.com>
2019-04-16 17:50:05 +0100

    e4

--- ana.py
+++ ana.py
@@ -102,7 +102,7 @@ class bote(discord.Client):
                 await channel.send("\U0001F44D We'll get that to you ASAP!")
 
         if m.startswith('translate'):
-            lang = m[10:]
+            lang = m[10:11]
             todo = m[13:]
             if len(lang) is not 2:
                     raise ValueError
