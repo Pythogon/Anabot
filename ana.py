ced02c249e5735339f20ff01a03662834eb73004
iCielin <sky@sirlovespikachu.uk>
2020-03-19 16:09:22 +0000

    Added something to make Dunc happy

--- ana.py
+++ ana.py
@@ -635,7 +635,8 @@ def getRate(l, user, prefix):
     2: ["Sorry, {}... but I'll pass.",0xfffb00,'★★☆☆☆'],
     3: ["{} is okay, that's all I can really say about them.", 0xffc100,'★★★☆☆'],
     4: ["I think {} is great.", 0xff5900,'★★★★☆'],
-    5: ['{} is literal fire~',0xff3131,'★★★★★']}.get(l)
+    5: ['{} is literal fire~',0xff3131,'★★★★★'],
+    6: ['{} is too cute for words~'],0xff0000,'★★★★★★'}.get(l)
     embed = discord.Embed(title = varset[0].format(user.name), color = varset[1])
     embed.add_field(name = f'Rating: {varset[2]}', value = f'Do you want to know what I think about someone? Do {prefix}rate [@user].')
     return embed
